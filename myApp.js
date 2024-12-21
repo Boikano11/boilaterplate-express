a9640da35d006d225149f038f92fafd1ffc7bf9b
Tsholofetso Pooe <pooetsholofetso@gmail.com>
2024-12-21 10:35:11 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,44 +1,13 @@
 let express = require('express');
 let app = express();
+const path = require('path');
 
+// Serve static files from the /public path
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res)=>{
     const filePath = __dirname + '/views/index.html';
-    res.sendFile(filePath);
+    res.sendFile(filePath);    
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
  module.exports = app;
