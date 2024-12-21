3d8c850a71aa5241cf5df008530c15408836742a
Tsholofetso Pooe <pooetsholofetso@gmail.com>
2024-12-21 10:15:44 +0000

    Add file path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.get('/', (req, res)=>{
+    const filePath = __dirname + '/views/index.html';
+    res.sendFile(filePath);
+});
+
 
 
 
